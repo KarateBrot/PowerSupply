70ec771a22b790e3f30688463fd640de4010ba10
KarateBrot <Rm2k-Freak@web.de>
2018-01-26 21:40:50 +0100

    implemented PID-controller

--- src/Vaporizer.cpp
+++ src/Vaporizer.cpp
@@ -17,17 +17,19 @@
 Sensor::Sensor()
 {
   _INA219.begin();
+  _INA219.setCalibration_32V_2A();
+  setTCR(TCR_SS316L);
 }
 
 void Sensor::read()
 {
-  // current [mA]
+  // Current [mA]
   current = (double)_INA219.getCurrent_mA()*2.0;   // R050 instead of R100 shunt
 
-  // voltage [mV]
+  // Voltage [mV]
   voltage = (double)_INA219.getBusVoltage_V()*1000.0;
 
-  // resistance [Ω]
+  // Resistance [Ω]
   resistance =
     ( 9.0*resistance + (voltage/constrain(current, 1, 15000) - _resCable) )/10.0;
 
@@ -35,12 +37,12 @@ void Sensor::read()
     resistance = _res20;
   }
 
-  // power [W]
+  // Power [W]
   power = (float)(power + voltage*current/1000000.0)/2.0f;
 
-  // temperature [°C]
+  // Temperature [°C]
   temperature =
-    ( 95.0f*temperature + (float)( TCR_SS316L*log(resistance/_res20) + 20.0 )*5.0f )/100.0f;
+    ( 95.0f*temperature + (float)( _TCR*log(resistance/_res20) + 20.0 )*5.0f )/100.0f;
 }
 
 // -------------------------------- SENSOR ---------------------------------- //
@@ -55,6 +57,7 @@ DAC::DAC()
   _MCP4725.setVoltage(4095, false);            // boot with minimal power output
 }
 
+// Sets DAC pin "OUT" to DC voltage from 0..Vcc
 void DAC::setOutput(uint16_t val)
 {
   _MCP4725.setVoltage(4095 - val, false);
@@ -73,29 +76,53 @@ Heater::Heater(float p, float i, float d)
   _d = d;
 }
 
-void Heater::set_PID(float p, float i, float d)
-{
-  _p = p;
-  _i = i;
-  _d = d;
-}
-
 // Make sure heater core is at room temperature before calibration!
 void Heater::calibrate()
 {
   for (size_t i = 0; i < 30; i++) {
     sensor.read();
   }
-  sensor.set_res20(sensor.resistance);
+  sensor.setRes20(sensor.resistance);
 
   // <-- Insert PID-tuning here
 
-  // set_PID(p, i, d);
+  // setPID(p, i, d);
 }
 
+// Sets DAC pin "OUT" to DC voltage according to PID-controller
 void Heater::regulate()
 {
-  
+  // ------------------------------------------------------------------------ //
+  //                         --< PID-CONTROLLER >--                           //
+  // -------------------------------------------------------------------------//
+  // SIGNAL OUTPUT:   u(t) = P*e(t) + I*∫e(t)dt + D*de(t)/dt                  //
+  //                      e(t)     = ΔT                                       //
+  //                      ∫e(t)dt  = dac_idle                                 //
+  //                      de(t)/dt = -dT/dt                                   //
+  //                      u(t)     = dac_output                               //
+  //            =>    dac_output   = P*ΔT + I*dac_idle - D*dT/dt              //
+  // ------------------------------------------------------------------------ //
+
+  // ΔT [°C]
+  _dTemp = (float)temperature_set - sensor.temperature;
+
+  // Time step for I and D
+  float _dt = (micros() - _timeLast)/1000000.0;
+  _timeLast = micros();
+
+  // dT/dt [°C/s]
+  _dTdt     = (sensor.temperature - _tempLast)/_dt;
+  _tempLast = sensor.temperature;
+
+  // ∫ΔTdt [°C*s]
+  if (_dTemp <= 5.0) {                                                          // only start integrating shortly before reaching ΔT = 0 (to prevent T from overshooting)
+    _idle += _dTemp*_dt + 0.05;                                                 // [+ 1.0]: let P and I fight each other (for "stiffer" temp regulation)
+    _idle  = constrain(_idle, 0, 4095);
+  }
+
+  _output = (uint16_t)constrain(_p*_dTemp + _i*_idle - _d*_dTdt, 0, 4095);
+
+  dac.setOutput(_output);
 }
 
 // --------------------------------- Heater --------------------------------- //
--- src/Vaporizer.h
+++ src/Vaporizer.h
@@ -23,13 +23,29 @@
 // -------------------------------------------------------------------------- //
   #define TCR_SS316L 0.00092      // at 20°C                                  //
 // -------------------------------------------------------------------------- //
+  #define PID_P      850.0f                                                   //
+  #define PID_I      100.0f                                                   //
+  #define PID_D       80.0f                                                   //
+// -------------------------------------------------------------------------- //
+
+
+
+class Service
+{
+  uint32_t _time = 0;
+
+public:
+
+  void     startRuntime(void)       { _time = micros(); }
+  uint32_t getRuntime  (void) const { return micros() - _time; };
+};
 
 
 
 class Sensor
 {
   Adafruit_INA219 _INA219;
-  double          _res20, _resCable;
+  double          _TCR, _res20, _resCable;
 
  public:
 
@@ -38,9 +54,10 @@ class Sensor
 
   Sensor(void);
 
-  void read        (void);
-  void set_res20   (double);
-  void set_resCable(double);
+  void read       (void);
+  void setRes20   (double res) { _res20    = res; }
+  void setResCable(double res) { _resCable = res; }
+  void setTCR     (double tcr) { _TCR      = tcr; }
 };
 
 
@@ -60,16 +77,22 @@ class DAC
 
 class Heater
 {
-  float _p, _i, _d;
+  float    _p, _i, _d;
+  float    _dTemp, _dt, _timeLast, _dTdt, _tempLast, _idle;
+  uint16_t _output;
 
  public:
 
-  Sensor sensor;
-  DAC    dac;
+  Sensor   sensor;
+  DAC      dac;
+  uint16_t temperature_set;
 
   Heater(float, float, float);
 
-  void set_PID  (float, float, float);
+  void setPID   (float p, float i, float d) { _p = p; _i = i; _d = d; }
+  void setPID_P (float p)                   { _p = p; }
+  void setPID_I (float i)                   { _i = i; }
+  void setPID_D (float d)                   { _d = d; }
   void calibrate(void);
   void regulate (void);
 };
@@ -94,7 +117,9 @@ class GUI
 
 class Vaporizer
 {
+ public:
 
+   Heater heater;
 };
 
 // -------------------------------- VAPORIZER ------------------------------- //
